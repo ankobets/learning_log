7383a597982fbf696dd87482448f76f38a8e13d5
ankobets <ank4kobets.yandex.ru>
2025-02-07 00:20:56 +0300

    Add index.html and edit urls.py

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -21,9 +21,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls'))
 ]
-
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
