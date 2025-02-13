d3857cf447d90bb0b6d7707e518b6e1b64a0356b
ankobets <ank4kobets.yandex.ru>
2025-02-13 15:18:43 +0300

    Add entry form

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -1,8 +1,17 @@
 from django import forms
-from .models import Topic
+from .models import Topic, Entry
+
 
 class TopicForms(forms.ModelForm):
     class Meta:
         model = Topic
         fields = ['text']
         labels = {'text': ''}
+
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model = Entry
+        fields = ['text', 'ex_data']
+        labels = {'text': 'Entry:', 'ex_data': 'Your image:'}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
