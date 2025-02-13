5e9c210ce765d99e48e11351ce858b62261a1f20
ankobets <ank4kobets.yandex.ru>
2025-02-13 16:32:22 +0300

    Edit new_entry view and add new_entry.html

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 
-
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from .models import Topic
-from .forms import TopicForms
+from django.urls import reverse_lazy
 
+from .models import Topic, Entry
+from .forms import TopicForms, EntryForm
+from django.views.generic import CreateView
 
 
 # Create your views here.
@@ -34,5 +36,24 @@ def new_topic(request):
             return redirect('learning_logs:topics')
 
     context = {'form': form}
-    return render(request, 'learning_logs/new_topic.html', context )
+    return render(request, 'learning_logs/new_topic.html', context)
 
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method == 'POST':
+        form = EntryForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            # Getting the current instance object to display in the template
+            img_object = form.instance
+
+            return redirect('learning_logs:topic', topic_id=topic_id)
+    else:
+        form = EntryForm()
+
+    return render(request, 'learning_logs/new_entry.html', {'topic': topic, 'form': form})
