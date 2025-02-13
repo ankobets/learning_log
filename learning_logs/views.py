7a6ad605635775e4a518ccd3a278ec2dd3525333
ankobets <ank4kobets.yandex.ru>
2025-02-13 22:50:47 +0300

    Added entry editing functionality

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -57,3 +57,25 @@ def new_entry(request, topic_id):
         form = EntryForm()
 
     return render(request, 'learning_logs/new_entry.html', {'topic': topic, 'form': form})
+
+
+def edit_entry(request, entry_id):
+
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method == 'POST':
+        form = EntryForm(request.POST, request.FILES, instance=entry)
+        if form.is_valid:
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    else:
+        form = EntryForm(instance=entry)
+
+    context = {'entry': entry, 'topic':topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+
+
+
+
